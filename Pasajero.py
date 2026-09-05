ebdc919d637248f8c3ba5ec1802282cb554755ee
benzagon <gonbenzaquen@gmail.com>
2026-09-05 19:47:37 -0300

    Pasajeros

---
+++ Pasajero.py
@@ -0,0 +1,160 @@
+import random
+from enum import Enum, auto
+
+TIEMPO_PARARME = 3
+
+class Estado(Enum):
+    SENTADO = auto()
+    SENTANDOME = auto()
+    PARADO = auto()
+    AVANZANDO = auto()
+    LISTENING_AYO = auto()
+    DOING_AYO = auto()
+    ESPERANDO_VOLVLER_AYO = auto()
+
+class Pasajero:
+    def __init__(self, asiento, P, T_SENTADO, T_CARRYON):
+        self.carryOn = random.uniform(0, 1) <= P
+        self.asiento = asiento
+        self.pos = [0,2]
+        self.soyVentana = asiento[1] == 4 or asiento[1] == 0
+
+        self.estado = Estado.PARADO
+
+        self.HICE_AYO = False
+        self.estoy_esperando = False
+        self.tiempoAEsperar = 0
+
+        self.T_ACOMODARME = T_SENTADO + self.carryOn * T_CARRYON
+
+    # Estoy a un asiento
+    def estoyAUno(self):
+        return self.pos[0] == self.asiento[0] - 1
+
+    def llegueAFila(self):
+        return self.pos[0] == self.asiento[0]
+
+    def sentarme(self, avion):
+        avion[self.asiento[0]][self.asiento[1]] = 1
+
+        if self.HICE_AYO:
+            avion[self.asiento[0]][2] = 8
+        else:
+            avion[self.asiento[0]][2] = 0
+
+        self.pos = self.asiento
+
+        if not self.soyVentana:
+            if (self.asiento[1] == 1 and avion[self.asiento[0]][0] != 1) or (self.asiento[1] == 3 and avion[self.asiento[0]][4] != 1):
+                self.estado = Estado.LISTENING_AYO
+        else:
+            self.estado = Estado.SENTADO
+        return
+
+    def avanzar(self, avion):
+        avion[self.pos[0]][self.pos[1]] = 0
+        self.pos[0]+=1
+        avion[self.pos[0]][self.pos[1]] = 1
+
+        self.estado = Estado.PARADO
+        return
+
+    def tengoAlguienEnFrente(self, avion):
+        return avion[self.pos[0] + 1][2] != 0
+
+    def asientoPasilloOcupado(self, avion):
+        if self.asiento[1] == 4:
+            return avion[self.asiento[0]][3]
+        return avion[self.asiento[0]][1]
+
+    def decidirObjetivo(self, avion):
+        # Esperar
+        self.tiempoAEsperar -= 1
+        if self.tiempoAEsperar > 0: return
+
+        pass #
+
+    def analizarPARADO(self):
+        if self.tengoAlguienEnFrente(): return
+        if not self.estoy_esperando:
+            self.tiempoAEsperar = 3
+            self.estoy_esperando = True
+        else:
+            self.estado = Estado.AVANZANDO
+            self.estoy_esperando = False
+        return
+
+    def analizarAVANZANDO(self, avion):
+        if self.soyVentana and self.estoyAUno() and self.asientoPasilloOcupado():
+            # Avisarle que se pare EYO ESTOOO TIENE QYE SER 3 A VECES
+            #####
+            avion[self.asiento[0]][2] = 2
+            self.estado = Estado.PARADO
+            self.HICE_AYO = True
+            return
+
+        if self.llegueAFila():
+            self.estado = Estado.SENTANDOME
+            return
+
+        if not self.estoy_esperando:
+            if self.carryOn:
+                self.tiempoAEsperar = 6
+            else:
+                self.tiempoAEsperar = 3
+            self.estoy_esperando = True
+            return
+
+        self.estoy_esperando = False
+        self.avanzar(avion)
+        return
+
+    def analizarSENTARME(self, avion):
+        if not self.estoy_esperando:
+            self.tiempoAEsperar = self.T_ACOMODARME
+            self.estoy_esperando = True
+
+        self.estoy_esperando = False
+        self.sentarme(avion)
+        return
+
+    def analizarLISTENING_AYO(self, avion):
+        # Esperando a que me avisen AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        if avion[self.pos[0]][2] != 2: return
+
+        if not self.estoy_esperando:
+            self.tiempoAEsperar = self.T_PARARME
+            self.estoy_esperando = True
+
+        self.estoy_esperando = False
+        avion[self.pos[0]][self.pos[1]] = 0
+        self.pos[1] = 2
+        avion[self.pos[0]][self.pos[1]] = 1
+        self.estado = Estado.DOING_AYO
+        return
+
+    def analizarDOING_AYO(self, avion):
+        if self.tengoAlguienEnFrente(): return
+        if not self.estoy_esperando:
+            self.tiempoAEsperar = 3
+            self.estoy_esperando = True
+        else:
+            self.estoy_esperando = False
+            # MUEVO UNO ADELANTE
+            avion[self.pos[0]][self.pos[1]] = 0
+            self.pos[0]+=1
+            avion[self.pos[0]][self.pos[1]] = 1
+            self.estado = Estado.ESPERANDO_VOLVLER_AYO
+        return
+
+    def analizarESPERANDO_VOLVER_AYO(self, avion):
+        # Esperando a que me avisen AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        if avion[self.pos[0]][2] != 8: return
+
+        if not self.estoy_esperando:
+            self.tiempoAEsperar = 6
+            self.estoy_esperando = True
+
+        self.estoy_esperando = False
+        self.sentarme()
+        return
---
+++ main.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+N = 25
